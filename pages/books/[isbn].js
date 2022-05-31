65739e2e0a8a13bc7f5cfa3a49d17891fe3347af
Mingxia Li <limingxia07@gmail.com>
2022-05-31 14:29:15 +1000

    book card component

--- pages/books/[isbn].js
+++ pages/books/[isbn].js
@@ -1,23 +1,11 @@
-import {useRouter} from 'next/router'
-import Link from 'next/link'
 import {getBookByIsbn} from "../../lib/fetchBook";
-
-const Code = (p) => <code {...p} />
+import BookCard from "../../components/BookCard";
 
 const Books = ({ book }) => {
-    const { asPath, route, query } = useRouter()
+    const bookObj = JSON.parse(book)
+    // refine data
     return (
-        <div>
-            <h1>Path: {asPath}</h1>
-            <hr/>
-            <p>This page is rendered by <Code>{`pages${route}.js`}</Code></p>
-            <p>This query <Code>isbn</Code> for this page is: {' '}
-                <Code>{JSON.stringify(query.isbn)}</Code>
-            </p>
-            <Link href="/">
-                <a> Back Home</a>
-            </Link>
-        </div>
+        <BookCard {...bookObj} />
     )
 }
 
