60fec5146f98fe39f3e8cde0491f6bd31e5e3fd0
Mingxia Li <limingxia07@gmail.com>
2022-05-31 11:15:38 +1000

    config mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
-import '../styles/globals.css'
+import * as React from 'react'
+import {ThemeProvider} from "@mui/material/styles";
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../components/createEmotionCache";
 import Head from 'next/head'
+import theme from "../components/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <>
-    <Head>
-      <title>Cool kids' library</title>
-    </Head>
-    <Component {...pageProps} />
-  </>
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Cool kids' library</title>
+        <meta name="viewpoint" content={"initial-scale=1, width=device-width"} />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,35 @@
 import NextDocument, {Html, Head, Main, NextScript } from 'next/document'
+import createEmotionCache from "../components/createEmotionCache";
+import createEmotionServer from '@emotion/server/create-instance';
+import theme from "../components/theme";
 
 export default class MyDocument extends NextDocument {
-    static getInitialProps(ctx) {
-        return NextDocument.getInitialProps(ctx)
+    static async getInitialProps(ctx) {
+        const originalRenderPage = ctx.renderPage
+        const cache = createEmotionCache()
+        const { extractCriticalToChunks } = createEmotionServer(cache)
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) =>
+                    function EnhanceApp(props) {
+                        return <App emotionCache={cache} {...props} />
+                    },
+            })
+        const initialProps = await NextDocument.getInitialProps(ctx)
+        const emotionStyles = extractCriticalToChunks(initialProps.html)
+        const emotionStyleTags = emotionStyles.styles.map( style =>
+            <style
+                data-emotion={`${style.key} ${style.ids.join(' ')}`}
+                key={style.key}
+                dangerouslySetInnerHTML={{__html: style.css}}
+            />
+        )
+
+        return {
+            ...initialProps,
+            emotionStyleTags,
+        }
     }
 
     render() {
@@ -10,13 +37,20 @@ export default class MyDocument extends NextDocument {
             title: "Library for kids",
             description: "Easy access to best picture books"
         }
+        const { emotionStyleTags } = this.props
         return (
             <Html lang="en">
                 <Head>
                     <meta charSet="utf-8"/>
                     <meta name="robots" content="follow, index" />
                     <meta name="description" content={meta.description} />
+                    <meta name="theme-color" content={theme.palette.primary.main} />
                     <link rel="icon" href="/favicon.ico" />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                    { emotionStyleTags }
                 </Head>
 
                 <body>
